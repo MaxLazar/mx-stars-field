7512e2b988f999aa2e725fb6fbae97b4d6d9f7e7
Max Lazar <max@eec.ms>
2013-10-17 00:28:24 +0400

    ini

---
+++ README.md
@@ -0,0 +1,3 @@
+# MX Stars Field #
+
+[Documentation](http://www.eec.ms/add-ons/mx-stars-field)
---
+++ system/expressionengine/third_party/mx_stars_field/config.php
@@ -0,0 +1,18 @@
+<?php
+if (! defined('MX_STARS_FIELD_KEY'))
+{
+	define('MX_STARS_FIELD_NAME', 'MX Stars Field');
+	define('MX_STARS_FIELD_VER',  '2.7.0');
+	define('MX_STARS_FIELD_KEY', 'mx_stars_field');
+	define('MX_STARS_FIELD_AUTHOR',  'Max Lazar');
+	define('MX_STARS_FIELD_DOCS',  'http://www.eec.ms/add-on/mx-stars-field');
+	define('MX_STARS_FIELD_DESC',  '');
+
+}
+
+/*
+ * NSM Addon Updater
+$config['name'] = MX_STARS_FIELD_NAME;
+$config['version'] = MX_STARS_FIELD_VER;
+$config['nsm_addon_updater']['versions_xml'] = 'http://www.eec.ms/versions/mx-select-plus';
+*/
